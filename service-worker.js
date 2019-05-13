02adca841ad4293f417c19b9f4345a12e811503a
waxime <aveline.maxime@gmail.com>
2019-05-13 15:21:48 +0200

    service-worker: Install service worker with cached files and fetch the cache

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,33 @@
 console.log('In service worker');
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches.open('v1').then(cache => {
+        cache.addAll([
+          '/',
+          'index.html',
+          '/js/app.js',
+          '/css/app.css'
+        ]);
+      }),
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method === 'GET') {
+    event.respondWith(
+        caches.match(event.request).then(function(resp){
+          return resp || fetch(event.request).then(function(response){
+            let responseClone = response.clone();
+            caches.open('v1').then(function(cache){
+              cache.put(event.request, responseClone);
+            });
+
+            return response;
+          }).then(function(){
+            return self.skipWaiting();
+          })
+        })
+    );
+  }
+});
